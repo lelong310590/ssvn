08257fa95b84aab100349fcbe2318230c82fb7a7
Train Heartnet <longlengoc90@gmail.com>
2021-07-09 17:49:56 +0700

    update phoi

--- module/course/resources/views/frontend/certificate.blade.php
+++ module/course/resources/views/frontend/certificate.blade.php
@@ -82,7 +82,7 @@
             <h1 class="text-center text-uppercase" style="color: red; font-weight: 900;">Giấy chứng nhận đào tạo</h1>
             <div class="certificate-info">
                 <p><span style="color: green; text-transform: uppercase; font-size: 23px; margin-left: 154px;">Sở y tế tỉnh Bình Dương</span></p>
-                <p>Chứng nhận: <b style="margin-left: 25px">{{$user->sex == 'male' ? 'Ông' : 'Bà'}} {{$user->first_name}}</b></p>
+                <p>Chứng nhận: <b style="margin-left: 25px">{{$user->sex == 'male' ? 'Ông' : 'Bà'}} {{$user->first_name}} {{$user->last_name}}</b></p>
                 <p>Đơn vị: <b style="margin-left: 25px">{{$company != null ? $company->name : ''}}</b></p>
                 <p>Đã hoàn thành khóa huấn luyện đào tạo:</p>
                 <p class="text-center text-uppercase" style="font-size: 18px">{{$course->name}}</p>
