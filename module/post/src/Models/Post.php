0e7f43a708a386c011c56dec5e0eab642859da90
Train Heartnet <longlengoc90@gmail.com>
2021-08-04 18:58:52 +0700

    update

--- module/post/src/Models/Post.php
+++ module/post/src/Models/Post.php
@@ -16,7 +16,7 @@ class Post extends Model
     protected $table = 'post';
     protected $fillable = [
         'name', 'slug', 'content', 'thumbnail', 'author', 'post_type', 'template', 'created_at', 'updated_at', 'seo_title',
-        'seo_keywords', 'seo_description', 'excerpt', 'status', 'orders'
+        'seo_keywords', 'seo_description', 'excerpt', 'status', 'order'
     ];
 
     public function getAuthor()
